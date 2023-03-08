f3a03da8837c258247c389371197c4cbbedad003
jaasiel17 <jaasielguerra17@gmail.com>
2023-03-07 20:31:50 -0600

    Creación proyecto

---
+++ pvsimple/src/main/java/com/guerra/pvsimple/PvSimpleApplication.java
@@ -0,0 +1,15 @@
+package com.guerra.pvsimple;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log
+public class PvSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PvSimpleApplication.class, args);
+    }
+
+}
---
+++ pvsimple/src/test/java/com/guerra/pvsimple/PvSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guerra.pvsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
